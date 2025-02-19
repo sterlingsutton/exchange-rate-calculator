39ffd50f0d504ca3b26480f33c281f51d17f3e0a
Sterling Sutton <113621387+sterlingsutton@users.noreply.github.com>
2025-02-19 17:13:36 +0000

    Figure out how to parse data from API

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
+use std::collections::HashMap;
 use reqwest;
+use serde::{Serialize, Deserialize};
+use serde_json;
+
+#[derive(Serialize, Deserialize)]
+struct Currency {
+    base: String,
+    date: String,
+    rates: HashMap<String, f32>,
+}
 
 #[tokio::main]
 async fn main() {
-    let x = reqwest::get("https://api.frankfurter.dev/v1/latest?base=USD").await.unwrap().text().await.unwrap();
-    println!("{}", x);
+    let currency: Currency = serde_json::from_str(reqwest::get("https://api.frankfurter.dev/v1/latest").await.unwrap().text().await.unwrap().as_str()).unwrap();
+    let usd = currency.rates.get("USD").unwrap();
+    println!("{}", usd);
 }
