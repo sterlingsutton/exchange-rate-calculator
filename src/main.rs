d983dc9c42a45a04d809c58f99540889dcc75e33
Sterling Sutton <113621387+sterlingsutton@users.noreply.github.com>
2025-04-07 07:57:25 -0700

    Remove comments for AP CSP project reference

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-// Copyright (c) 2025 Sterling Sutton
-
 use std::collections::HashMap;
 use reqwest;
 use serde_json::{self, Value};
@@ -14,11 +12,8 @@ struct Exchange {
 impl Exchange {
     async fn calculate_target_amount(&self) -> f32 {
         let url = format!("https://api.frankfurter.dev/v1/latest?base={}", self.base);
-
         let data: Value = serde_json::from_str(reqwest::get(url).await.unwrap().text().await.unwrap().as_str()).unwrap();
-
         let rate = data["rates"][&self.target].as_f64().unwrap() as f32;
-
         rate * self.amount
     }
 
@@ -38,37 +33,30 @@ async fn main() {
     let exchange = get_exchange_info(&currencies);
     let target_amount = exchange.calculate_target_amount().await;
     println!("Target Amount ({}) : {:.2}", exchange.target, target_amount);
-
 }
 
 fn list_currencies(currencies: &HashMap<String, String>) {
     println!("---- AVAILABLE CURRENCIES ----");
     for (symbol, name) in currencies {
-        //let name = name.to_uppercase();
         println!("{} : {}", symbol, name);
     }
     println!();
 }
 
 fn get_exchange_info(currencies: &HashMap<String, String>) -> Exchange {
-    // Set default exchange values
     println!("---- EXCHANGE INFO ----");
     let mut exchange = Exchange::new();
-    // get base
     loop {
         print!("Base Currency : ");
         let base: String = text_io::read!();
-        // change base and break from loop if base is valid
         if currencies.contains_key(&base) {
             exchange.base = base;
             break;
         }
     }
-    // get amount
     loop {
         print!("Base Amount ({}) : ", exchange.base);
         let amount: Result<f32, Error> = text_io::try_read!();
-
         match amount.is_ok() {
             true => {
                 let amount = amount.unwrap();
@@ -77,23 +65,16 @@ fn get_exchange_info(currencies: &HashMap<String, String>) -> Exchange {
                     break;
                 }
             }
-            false => {
-                // do nothing
-            }
+            false => {}
         }
-
-        
     }
-    // get target
     loop {
         print!("Target Currency : ");
         let target: String = text_io::read!();
-        // same thing as with the base
         if currencies.contains_key(&target) && !target.eq(&exchange.base) {
             exchange.target = target;
             break;
         }
     }
-
     exchange
 }
